378eb0fbf93844c873fb4fa221cae6b76f8723ee
mos3448 <me9799253@gmail.com>
2025-03-20 08:23:17 -0700

    project7

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Assignment7 is ERC20 {
+ 
+contract Assignment7 is ERC20 { 
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor(string memory name,string memory symbol) ERC20("name","symbol"){} 
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address to,uint256 amount) external {
+        _mint(to,amount);
         // Fill in the logic
         // call _mint function with to and amount params
     }
